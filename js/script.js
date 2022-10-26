d6ee1ced5032d70cad6c1af874ad8a6d41a38be5
MarBozzz <mbozzuto@libero.it>
2022-10-26 17:41:23 +0200

    [updated]

--- js/script.js
+++ js/script.js
@@ -18,36 +18,69 @@ E se volessi un bottone per invertire la “direzione” del carosello?
 const carouselData = [
   {
     titolo : 'Argentina',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat odit illo voluptas? Reiciendis aperiam est, quia impedit illum atque reprehenderit obcaecati libero at',
-    image : 'img/argentina.jpg'
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitatione',
+    image : 'argentina.jpg'
   },
   {
     titolo : 'Chile',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat odit illo voluptas? Reiciendis aperiam est, quia impedit illum atque reprehenderit obcaecati libero at',
-    image : 'img/chile.jpg'
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque',
+    image : 'chile.jpg'
   },
   {
     titolo : 'Colombia',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat odit illo voluptas? Reiciendis aperiam est, quia impedit illum atque reprehenderit obcaecati libero at',
-    image : 'img/colombia.jpg'
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image : 'colombia.jpg'
   },
   {
     titolo : 'Peru',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat odit illo voluptas? Reiciendis aperiam est, quia impedit illum atque reprehenderit obcaecati libero at',
-    image : 'img/peru.jpg'
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat',
+    image : 'peru.jpg'
   },
   {
     titolo : 'Sweden',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat odit illo voluptas? Reiciendis aperiam est, quia impedit illum atque reprehenderit obcaecati libero at',
-    image : 'img/sweden.jpg'
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image : 'sweden.jpg'
   }
 ];
 
+const firstContainer = document.querySelector('.first-container');
 
 //ciclo forEach di lettura
+//console.log(`${element.titolo} e ${element.descrizione} e ${element.image}`);
+
 carouselData.forEach((element) => {
-  console.log(`${element.titolo} e ${element.descrizione} e ${element.image}`);
+  const country = element.titolo;
+  const text = element.descrizione;
+  const photo = element.image;
+  const container = `
+  <div class="container">
+  
+  <div class="slider">
+      <img class="" src="img/${photo}" alt="Foto1">
+      <div class="text">
+        <h2 class="title">${country}</h2>
+        <p class="description">${text}</p>
+      </div>
+  </div>
+
+  <div class="h-carousel d-flex">
+      <img class="" src="img/${photo}" alt="Foto1">
+      <img class="" src="img/${photo}" alt="Foto2">
+      <img class="" src="img/${photo}" alt="Foto3">
+      <img class="" src="img/${photo}" alt="Foto4">
+      <img class="" src="img/${photo}" alt="Foto5">
+  
+      <div class="btn left">&larr;</div>
+      <div class="btn right">&rarr;</div>
+  </div>
+  
+  </div>`
+  
+  
+  firstContainer.innerHTML = container;
 });
 
-const container = document.querySelector('.container');
 
+
+//container.innerHTML = carouselData;
+//console.log(carouselData);
