99b2c00ff7e6dd7bd2b294e58f4f46ba99f03293
MarBozzz <mbozzuto@libero.it>
2022-10-26 14:45:58 +0200

    [init]

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+**Consegna:**
+Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando un array di oggetti.
+Ogni elemento deve avere un titolo, una descrizione e il riferimento ad una immagine.
+immagini devono essere 5 e nella grafica devono essere presenti:
+- immagine in evidenza
+- thumbnail di tutte le immagine con in evidenza l’immagine attiva
+- bottone avanti e indietroLo screenshot allegato è solo dimostrativo ma cercate di mettere le thumb in basso e non laterlamente
+**Bonus 1:**
+Sperimentiamo attraverso l’uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:al click di un bottone o già dall’inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+****
+**Bonus 2:**
+E se volessi un bottone per invertire la “direzione” del carosello?
+****
+*/
+
+console.log('ciao'); 
